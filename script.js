becfd8c9e486edab9fe3c4bc482bd24ac029228e
JosephMG <jodsmg023@gmail.com>
2024-10-17 08:13:54 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,75 @@
+// Espera a que el DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', function() {
+    // Selecciona todos los elementos con la clase 'item'
+    const items = document.querySelectorAll('.item');
+
+    items.forEach(item => {
+        item.addEventListener('mouseover', function(){
+
+            items.forEach(item => item.classList.remove('active'));
+            
+            this.classList.add('active')
+
+            // Obtener el valor del data-content del item seleccionado
+            const activeSection = this.getAttribute('data-content');
+            
+            if (window.innerWidth > 992) {
+                
+                // En pantallas grandes, muestra el contenido a la derecha
+                const sections = document.querySelectorAll('.content-section');
+                
+                sections.forEach(section => {
+                    section.classList.remove('active');
+                });
+                
+
+                // Mostrar la sección activa
+                document.getElementById(activeSection).classList.add('active');
+            } else {
+                
+                // En móviles, comportamiento tipo acordeón
+                const content = document.getElementById(`${activeSection}-content`);
+
+                // Si el contenido ya está visible, ocúltalo
+                if (content.classList.contains('active')) {
+                    content.classList.remove('active');
+                } 
+                
+                else {
+                    // Ocultar todo el contenido del acordeón
+                    const allContents = document.querySelectorAll('.item-accordion-content');
+                    allContents.forEach(section => section.classList.remove('active'));
+        
+                    // Mostrar el contenido seleccionado
+                    content.classList.add('active');
+                }
+            }
+        })
+    })
+
+    let lastScrollPosition = 0;
+    const navbar = document.getElementById("navbar");
+
+    window.addEventListener("scroll", function() {
+        let currentScrollPosition = window.pageYOffset;
+
+        if (currentScrollPosition > lastScrollPosition) {
+            // Cuando bajas el scroll, oculta el navbar
+            navbar.classList.add('navbar-hidden');
+            navbar.classList.remove('navbar-visible', 'navbar-transparent');
+        } else {
+            // Cuando subes el scroll, muestra el navbar
+            navbar.classList.remove('navbar-hidden');
+            navbar.classList.add('navbar-visible');
+
+            // Cuando el scrool llege arriba, desapescar el background
+            if (currentScrollPosition == 0){
+                navbar.classList.add('navbar-transparent');
+                navbar.classList.remove('navbar-visible');
+            }
+        }
+
+        // Actualiza la posición del último scroll
+        lastScrollPosition = currentScrollPosition;
+    });
+});
