00aacdca8d0ce8560f9d86218b2dbb0eb45c317b
jodsmg023@gmail.com <jodsmg023@gmail.com>
2024-10-18 21:51:39 -0500

    Arreglando el carrousel para modo responsive

--- script.js
+++ script.js
@@ -86,6 +86,26 @@ $(document).ready(function(){
         centerMode: true,
         dots: false,
         infinite: true, 
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2
+              }
+            }
+          ]
     });
 
 
